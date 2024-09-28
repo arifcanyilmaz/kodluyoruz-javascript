347771f0fd027ee53bbddea2e2ba128938f94ad1
Arifcan Yılmaz <wisemann1917@gmail.com>
2024-09-28 22:48:35 +0300

    aa

---
+++ js/01-hello-world.js
@@ -0,0 +1,5 @@
+// document.write('Merhaba Dunya'); --> Direkt ekrana html ile yazdırır.
+
+// alert("Merhaba Dunya") --> Bildirim olarak ekrana yazdırır.
+
+// console.log("Merhaba Dunya") --> Console a yazdırır.
