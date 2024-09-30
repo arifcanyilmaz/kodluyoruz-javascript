49d240f1399a19bd6d19af72a1e0baef9e9e7044
Arifcan Yılmaz <wisemann1917@gmail.com>
2024-09-30 19:07:01 +0300

    Ders 10'a kadar tamam.

---
+++ js/09-document-object-model.js
@@ -0,0 +1,7 @@
+console.log(document.location.pathname) //dosya yolunu gösterir.
+
+console.log(document.head) //head kısmını consola a getirir.
+console.log(document.body) //body kısmını consola a getirir.
+console.log(document.URL) //sayfanın urlsini yazdırır.
+
+document.body.style.backgroundColor = "red" 
---
+++ js/10-dom-icerisinden-oge-secimi.js
@@ -0,0 +1,20 @@
+let h2 = document.getElementsByTagName('h2') // taglere göre çağırmak iyi değil.
+// console.log(h2.title.innerHTML)
+
+let title = document.getElementById('title') // yukardaki ile aynı yere girdik ama bu
+title.innerHTML = "Degisen Bilgi"            // yöntem daha iyi.
+console.log(title.innerHTML)
+
+
+let link = document.querySelector("ul#list>li>a")
+link.innerHTML += " Degişti"
+// console.log(link.innerHTML) artık index.htmlde yazan link bilgisi yerine değisen link
+//yazıyor. tekrardan consolda yazdırmaya gerek yok.
+
+
+
+let sameLink = document.querySelector('#kodluyoruzLink')
+sameLink.innerHTML = "link bir kez daha değişti"
+link.style.color = "red" 
+link.classList.add('btn') //bu seçilen linke class ekledik.Eğer bootstrap bağlı 
+//olsaydı artık bir buton olacaktı.
