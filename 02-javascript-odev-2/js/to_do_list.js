ed772ad14765d9d5febfe15609819a3dce790872
Arifcan Yılmaz <wisemann1917@gmail.com>
2024-10-10 15:06:52 +0300

    ödev2 fixed

--- 02-javascript-odev-2/js/to_do_list.js
+++ 02-javascript-odev-2/js/to_do_list.js
@@ -21,7 +21,6 @@ function formHandler(event){
     const maxTaskLenght = 130
     const taskLenght = TASK.value.length
 
-
     if( taskLenght <= maxTaskLenght ){
         if ((TASK.value.trim() !== "")){
             newElement(TASK.value)
@@ -38,27 +37,39 @@ function formHandler(event){
 
 }
 
+//İşlemden Sonra Inputu Temizleme
+function resetItem(task){
+    task.value = ""
+}
+
 //Listeye Eleman Ekleme
-function newElement(task,index){
+function newElement(task,id){
     //Ekleme
     let liDOM = document.createElement('li')
     liDOM.className = "list-group-item d-flex justify-content-between align-items-center"; //
     liDOM.innerHTML = `<p class="text-dark mt-3 inText ">${task}</p>
     <button class="btn-close border-1" aria-label="Close">&times;</button> `
+    liDOM.setAttribute('data-id', id);
     listDOM.append(liDOM)
-    removeItem(liDOM)
-    isDone(liDOM,task)
+    removeItem(liDOM,id)
+    isDone(liDOM,id)
 }
 
-//Listeden Elemen Silme
-function removeItem(liDOM){
-    liDOM.querySelector('.btn-close').addEventListener('click',function(){
-        liDOM.remove()
-    })
+
+//Listeden Eleman Silme
+function removeItem(liDOM, index) {
+    liDOM.querySelector('.btn-close').addEventListener('click', function () {
+        // Listeden HTML elemanını silme
+        liDOM.remove();
+
+        // LocalStorage'den de bu görevi silme
+        removeFromLocalStorage(index);
+    });
 }
+
 
 //Listedeki Task Yapıldı mı Sonrası
-function isDone(liDOM,){
+function isDone(liDOM,id){
     liDOM.addEventListener('click',function(){
         //Rengi değişti
         liDOM.className += " bg-info"
@@ -76,31 +87,72 @@ function isDone(liDOM,){
             </svg>`;
             liDOM.insertAdjacentHTML('afterbegin', checkIcon)
         }
+        updateTaskStatus(id)
     })
+
 }
 
-//İşlemden Sonra Inputu Temizleme
-function resetItem(task){
-    task.value = ""
-}
 
 //LS'ye Verileri Kaydetme
 function saveToLocalStorage(task){
     let jobs = JSON.parse(localStorage.getItem('list')) || []
-    jobs.push({task})
+    jobs.push({id: Date.now(),task, isDone:false})
     localStorage.setItem('list',JSON.stringify(jobs))
-
+    loadItemsFromLocaleStorage();
 }
+
+
 
 //LS'den Verileri Çekme
 window.addEventListener('DOMContentLoaded',loadItemsFromLocaleStorage)
 function loadItemsFromLocaleStorage(){
+    listDOM.innerHTML = '';
     let jobs = JSON.parse(localStorage.getItem('list')) || []
-    jobs.forEach((job,index) => {
-        newElement(job.task,index)
-        console.log(job.task,index)
+    jobs.forEach((job,id) => {
+        //console.log("index",job,index)
+        newElement(job.task,job.id)
+        console.log(job.isDone)
+        if(job.isDone == true){
+            let liDOM = listDOM.children[id];
+            liDOM.className += " bg-info";
+            const inText = liDOM.querySelector('.inText');
+            inText.style.textDecoration = 'line-through';
+
+            if (!liDOM.querySelector('svg')) {
+                const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+                <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+                <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+                </svg>`;
+                liDOM.insertAdjacentHTML('afterbegin', checkIcon);
+            }
+        }
     });
+    
+    
 }
+
+//LS'den Veri Silme
+function removeFromLocalStorage(id) {
+    let jobs = JSON.parse(localStorage.getItem('list')) || [];
+    const updatedJobs = jobs.filter(job => job.id !== id); 
+
+    // Eğer index doğruysa diziden öğeyi sil
+    localStorage.setItem('list', JSON.stringify(updatedJobs));
+    
+    loadItemsFromLocaleStorage();
+}
+
+
+//LS Update
+function updateTaskStatus(id) {
+    let jobs = JSON.parse(localStorage.getItem('list')) || [];
+    const jobIndex = jobs.findIndex(job => job.id === id)
+    if (jobIndex !== -1) {
+        jobs[jobIndex].isDone = true;
+        localStorage.setItem('list', JSON.stringify(jobs));
+    }
+}
+
 
 //Çalışırken Öğrendiklerim
 //1-event çağırırken örneğin formHandler'ı () ile çağırırsak sayfa açılır açılmaz çalışır.
